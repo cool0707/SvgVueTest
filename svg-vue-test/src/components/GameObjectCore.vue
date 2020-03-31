6214965563b622b03df64096f2719e2831530d6c
るしふぁ <cool0707@gmail.com>
2020-03-31 11:04:07 +0000

    色々やってみたけどダメだわ

---
+++ svg-vue-test/src/components/GameObjectCore.vue
@@ -0,0 +1,159 @@
+<template>
+  <g :key="id" 
+    :transform="transform"
+    :filter="$data.$_bDragOver?'url(#myFilter2)':null"
+    @mousedown.exact.prevent ="moveStart()"
+    @touchstart.exact.prevent ="moveStart()"
+    @mousedown.shift.prevent ="toggleSelect()"
+    @mousedown.ctrl.prevent ="flip()"
+    @mouseenter.prevent ="dragEnter()"
+    @mouseleave.prevent ="dragLeave()"
+    @contextmenu.prevent >
+    <transition v-for="(f, fIdx) in $data.$_faces" :key="fIdx" name="slide-fade">
+      <image v-if="$data.$_faceIndex == fIdx"
+        preserveAspectRatio="none"
+        :xlink:href ="f.image"
+        :style ="{
+          transformOrigin: (f.w / 2) + 'px ' + (f.h / 2) + 'px',
+          pointerEvents: $data.$_bMoving ? 'none' : 'auto'}"     
+        :clip-path ="f.clipPath"
+        :x ="f.x" :y ="f.y" :width="f.imageWidth" :height="f.imageHeight" />
+    </transition>
+  </g>
+</template>
+
+<script>
+export default {
+  name: 'GameObject',
+  props: {
+    index: {
+      type: Number,
+      required: true
+    },
+    initX: {
+      type: Number,
+      default: 0
+    },
+    initY: {
+      type: Number,
+      default: 0
+    },
+    initAngle: {
+      type: Number,
+      default: 0
+    },
+    images: Array
+  },
+  computed: {
+    // trasformプロパティ
+    transform: function () {
+      return 'translate(' + [this.x, this.y].join(' ') + ') rotate(' + [this.angle, this.w/2, this.h/2].join(' ') + ')'
+    },
+    w: function () {
+      return this.$data.$_faces.length ? this.$data.$_faces[this.$data.$_faceIndex].w : 0
+    },
+    h: function () {
+      return this.$data.$_faces.length ? this.$data.$_faces[this.$data.$_faceIndex].h : 0
+    }
+  },
+  data: function () {
+    return {
+      id: this.index,
+      x: this.initX,
+      y: this.initY,
+      angle: this.initAngle,
+      $_bSelected: false,
+      $_bHidden: false,
+      $_bDragOver: false,
+      $_bMoving: false,
+      $_longTaptimer: null,
+      $_faces: [],
+      $_faceIndex: 0
+    } 
+  },
+  // マウス操作関連
+  mounted: function () {
+    console.log('MOUNT LISTENER ON')
+    this.$data.$_faces = []
+    this.images.forEach((i) => {
+      this.$data.$_faces.push({
+        w: i.width,
+        h: i.height,
+        x: -i.width * i.col,
+        y: -i.height * i.row,
+        image: i.source,
+        imageWidth: i.width * i.colSize,
+        imageHeight: i.height * i.rowSize,
+        clipPath: 'inset(' + [i.height * i.row, i.width * (i.colSize - i.col - 1), i.height * (i.rowSize - i.row - 1), i.width * i.col].join(' ') + ')',
+      })
+    })
+  },
+  beforeDestroy: function () {
+    console.log('MOUNT LISTENER OFF')
+    this.clearLongTap()
+  },
+  methods: {
+    dragEnter() {
+      //if (this.isMove) {
+        console.log('enter')
+        this.$data.$_bDragOver = true
+        this.startLongTap()
+      //}
+    },
+    dragLeave() {
+      //if (this.isMove) {
+        this.$data.$_bDragOver = false
+        this.clearLongTap()
+      //}
+    },
+    startLongTap() {
+      this.$data.$_longTaptimer = window.setTimeout(this.onLongTap, 800)
+    },
+    clearLongTap() {
+      if (this.$data.$_longTaptimer) {
+        window.clearTimeout(this.$data.$_longTaptimer)
+        this.$data.$_longTaptimer = null
+      }
+    },
+    onLongTap() {
+      console.log('tap')
+    },
+    select() {
+      this.$data.$_bSelected = true
+      this.$parent.move(this.id)
+    },
+    toggleSelect() {
+      this.$data.$_bSelected = !this.$data.$_bSelected
+    },
+    deSelect() {
+      this.$data.$_bSelected = false
+    },
+    // 図形を動かすフラグを立てる
+    moveStart() {
+      if (!this.$data.$_bSelected) {
+        this.select()
+      }
+      console.log('moveStart')
+      this.$data.$_bMoving = true
+      this.$parent.addMovingObject(this.move, this.moveEnd)
+      //document.addEventListener('mousemove', function (e) {console.log(e.x + ':' + e.y)})
+    },
+    // move中は前回まで動かした差分を取りながら座標を変化させていく
+    move(dx, dy) {
+      let tx = dx + Number(this.x)
+      let ty = dy + Number(this.y)
+      if (tx > 10 - this.w) this.x = tx
+      if (ty > 10 - this.h) this.y = ty
+    },
+    // マウスのクリックが終わった段階で初期化
+    moveEnd() {
+      this.$data.$_bMoving = false
+      this.clearLongTap()
+    },
+    flip() {
+      this.$data.$_faceIndex = (this.$data.$_faceIndex + 1) % this.$data.$_faces.length
+      this.$data.$_bReverse = !this.$data.$_bReverse
+    }
+  }
+}
+</script>
--- svg-vue-test/src/components/SvgDemo.vue
+++ svg-vue-test/src/components/SvgDemo.vue
@@ -1,47 +1,53 @@
 <template>
-  <div class="container">
+  <div class="container"
+    @mouseup.prevent="mouseUp($event)" @mousemove.prevent="mouseMove($event)"
+    @touchend.prevent="mouseUp($event)" @touchmove.prevent="mouseMove($event)">
     <!-- SVG定義 -->
     <svg :width="width" :height="height" :viewBox="viewport" @wheel="zoompan">
-      <filter id="myFilter">
+      <filter id="myFilter" filterUnits="userSpaceOnUse" x="-5" y="-5">
         <!-- 薄い灰色で領域を塗りつぶす。このレイヤーには「COLOR」と名前をつける -->
-        <feFlood flood-color="#ffbb00" result="COLOR"></feFlood>
+        <feFlood flood-color="#ffbb00" />
+        <feComposite in2="SourceAlpha" operator="in" result="BorderColor" />
 
-        <!-- DROPとCOLORレイヤーを合成し、シャドウに色を付ける。結果には「SHADOW」と名前をつける -->
-        <feComposite in="COLOR" in2="SourceAlpha" operator="in" result="SHADOW"></feComposite>
+        <feFlood flood-color="#000000" flood-opacity="0.8" />
+        <feComposite in2="SourceAlpha" operator="in" result="ShadowColor" />
+        
+        <feOffset in="BorderColor" dx="2" dy="2" result="Border1" />
+        <feOffset in="BorderColor" dx="2" dy="-2" result="Border2" />
+        <feOffset in="BorderColor" dx="-2" dy="2" result="Border3" />
+        <feOffset in="BorderColor" dx="-2" dy="-2" result="Border4" />
+        
+        <feMerge result="Border">
+          <feMergeNode in="Border1" />
+          <feMergeNode in="Border2" />
+          <feMergeNode in="Border3" />
+          <feMergeNode in="Border4" />
+        </feMerge>
 
-        <!-- SHADOWレイヤーを右下に20ピクセル分ずらす。新しいレイヤーには「DROPSHADOW」と名前をつける -->
-        <feOffset in="SHADOW" dx="2" dy="2" result="DROPSHADOW1"></feOffset>
-
-        <!-- SHADOWレイヤーを右下に20ピクセル分ずらす。新しいレイヤーには「DROPSHADOW」と名前をつける -->
-        <feOffset in="SHADOW" dx="2" dy="-2" result="DROPSHADOW2"></feOffset>
-
-        <!-- SHADOWレイヤーを右下に20ピクセル分ずらす。新しいレイヤーには「DROPSHADOW」と名前をつける -->
-        <feOffset in="SHADOW" dx="-2" dy="2" result="DROPSHADOW3"></feOffset>
-
-        <!-- SHADOWレイヤーを右下に20ピクセル分ずらす。新しいレイヤーには「DROPSHADOW」と名前をつける -->
-        <feOffset in="SHADOW" dx="-2" dy="-2" result="DROPSHADOW4"></feOffset>
-
-        <!-- 画像が上にくるように、DROPSHADOWとソース画像を重ね合わせる。（MergeNodeの順番が反映されることを忘れずに） -->
+        <feOffset in="ShadowColor" dy="5" />
+        <feGaussianBlur stdDeviation="10" result="DropShadow" />
+        
         <feMerge>
-            <feMergeNode in="DROPSHADOW1"></feMergeNode>
-            <feMergeNode in="DROPSHADOW2"></feMergeNode>
-            <feMergeNode in="DROPSHADOW3"></feMergeNode>
-            <feMergeNode in="DROPSHADOW4"></feMergeNode>
+            <feMergeNode in="Border"></feMergeNode>
+            <feMergeNode in="DropShadow"></feMergeNode>
             <feMergeNode in="SourceGraphic"></feMergeNode>
         </feMerge>
       </filter>
-      <rect :width="width" :height="height" x="0" y="0" fill="#225511" />
-      <g v-for="(o, oIdx) in objects" :key="oIdx" 
-        :transform="'translate(' + o.x + ' ' + o.y + ')'"
-        :filter="(selectIdx==oIdx)?'url(#myFilter)':null"
-        @mousedown.exact="move($event, oIdx)" @mousedown.ctrl="flipCard($event, oIdx)">
-        <transition v-for="(f, fIdx) in o.faces" :key="fIdx" name="slide-fade">
-          <image v-if="o.faceIndex == fIdx" xlink:href="http://blog-imgs-42.fc2.com/p/i/p/piposozai/cardss.png"
-            v-bind:style="{ transformOrigin: (f.w / 2) + 'px ' + (f.h / 2) + 'px' }"
-            preserveAspectRatio="none"
-            :clip-path="f.clipPath"
-            :x="f.x" :y="f.y" :width="o.w" :height="o.h" />
-        </transition>
+      <filter id="myFilter2" filterUnits="userSpaceOnUse">
+        <!-- 薄い灰色で領域を塗りつぶす。このレイヤーには「COLOR」と名前をつける -->
+        <feFlood flood-color="#ffbb00" flood-opacity="0.5" result="COLOR"></feFlood>
+        <feComposite in="COLOR" in2="SourceAlpha" operator="in" result="SHADOW"></feComposite>
+        <feMerge>
+          <feMergeNode in="SourceGraphic"></feMergeNode>
+          <feMergeNode in="SHADOW"></feMergeNode>
+        </feMerge>
+      </filter>
+      <rect :width="width" :height="height" x="0" y="0" fill="#225511"
+        @mousedown.exact="clearSelect()"
+        @contextmenu.prevent />
+      <GameObject v-for="(o) in unselected" :key="o.index" v-bind="o" />
+      <g filter="url(#myFilter)">
+        <GameObject v-for="(o) in selected" :key="o.index" v-bind="o" />
       </g>
       <!-- top right bottom left clip-path="inset(25% 73.33333% 50% 20%)" -->
     </svg>
@@ -49,13 +55,23 @@
 </template>
 
 <script>
+import GameObject from './GameObjectCore.vue'
+
 export default {
-    name: 'SVGDemo',
-    props: {
-        width: Number,
-        height: Number
-    },
-  data () {
+  name: 'SVGDemo',
+  props: {
+      width: Number,
+      height: Number
+  },
+  components: {
+    GameObject
+  },
+  provide: function () {
+    return {
+      startMove: this.startMove
+    }
+  },
+  data: function () {
     return {
       ratio: 1,
       dx: 0,
@@ -64,58 +80,66 @@ export default {
       isMove: false,
       beforeMouseX: null,
       beforeMouseY: null,
-      selectIdx: 0,
-      objects: []
+      lastTouchElement: null,
+      activeDroppable: null,
+      timer: null,
+      objects: [],
+      selected: [],
+      unselected: [],
+      movingObjectCallbacks: []
     } 
   },
   // マウス操作関連
-  mounted () {
+  mounted: function () {
     console.log('MOUNT LISTENER ON')
-    document.addEventListener('mouseup', this.mouseUp)
-    document.addEventListener('mousemove', this.mouseMove)
+    //document.addEventListener('mouseup', this.mouseUp)
+    //document.addEventListener('mousemove', this.mouseMove)
 
-    let w = 1200
-    let h = 400
+    let w = 80
+    let h = 100
 
     let cols = 15
     let rows = 4
 
-    let cellW = w / cols
-    let cellH = h / rows
-
     for (let i=0; i<cols; i++) {
       for (let j=0; j<rows; j++) {
         this.objects.push({
-          faces: [
+          index: this.objects.length,
+          images: [
             {
-              w: cellW,
-              h: cellH,
-              x: -cellW * i,
-              y: -cellH * j,
-              clipPath: 'inset(' + [cellH * j, cellW * (cols - i - 1), cellH * (rows - j - 1), cellW * i].join(' ') + ')',
+              source: 'https://blog-imgs-42.fc2.com/p/i/p/piposozai/cardss.png',
+              height: h,
+              width: w,
+              rowSize: rows,
+              colSize: cols,
+              row: j,
+              col: i
             },
             {
-              w: cellW,
-              h: cellH,
-              x: -cellW * (cols - 1),
-              y: -cellH * (rows - 1),
-              clipPath: 'inset(' + [cellH * (rows - 1), 0, 0, cellW * (cols - 1)].join(' ') + ')',
+              source: 'https://blog-imgs-42.fc2.com/p/i/p/piposozai/cardss.png',
+              height: h,
+              width: w,
+              rowSize: rows,
+              colSize: cols,
+              row: rows - 1,
+              col: cols - 1
             }
           ],
-          w: w,
-          h: h,
-          x: 0,
-          y: 0,
-          faceIndex: 0,
-          bReverse: false,
+          initX: 0,
+          initY: 0,
+          initAngle: 0
         })
       }
     }
+
+    // 選択状態配列を更新
+    this.updateSelect()
   },
-  beforeDestroy () {
+  beforeDestroy: function () {
     console.log('MOUNT LISTENER OFF')
-    document.removeEventListener('mouseup', this.mouseUp)
-    document.removeEventListener('mousemove', this.mouseMove)
+    //document.removeEventListener('mouseup', this.mouseUp)
+    //document.removeEventListener('mousemove', this.mouseMove)
+    this.clearLongTap()
   },
   methods: {
     zoompan (e) {
@@ -132,6 +156,10 @@ export default {
         this.makeViewBox(x, y, w, h)
         this.ratio = w / this.width
         e.preventDefault()
+      } else if (e.altKey) {
+        for(let obj of this.selected) {
+          obj.angle = (e.deltaY * 0.1 + Number(obj.angle) + 360) % 360
+        }
       } else {
         // 移動
         if ((this.dx + e.deltaX > -this.width && this.dy + e.deltaY > -this.width) &&
@@ -146,50 +174,160 @@ export default {
     makeViewBox (x, y, w, h) {
       this.viewport = [x, y, w, h].join(' ')
     },
+    dragEnter (i) {
+      if (this.isMove) {
+        //this.objects[i].bDropping = true
+        this.startLongTap(i)
+      }
+    },
+    dragLeave (i) {
+      if (this.isMove) {
+        this.objects[i].bDropping = false
+        this.activeDroppable = null
+        this.clearLongTap()
+      }
+    },
+    startLongTap (i) {
+      this.timer = window.setTimeout(this.longTap, 800, i)
+    },
+    clearLongTap () {
+      if (this.timer) {
+        window.clearTimeout(this.timer)
+        this.timer = null
+      }
+    },
+    longTap (i) {
+      this.objects[i].bDropping = true
+      this.activeDroppable = this.objects[i]
+    },
     // 図形を動かすフラグを立てる
-    move (e, i) {
+    move (i) {
       this.isMove = true
-      this.selectIdx = i
-      e.preventDefault()
+
+      if (!this.objects[i].bSelected) {
+        this.clearSelect()
+        this.objects[i].bSelected = true
+
+        // 選択状態配列を更新
+        this.updateSelect()
+      }
+    },
+    updateSelect () {
+      this.selected = this.objects.filter(o => !o.bHidden && o.bSelected)
+      this.unselected = this.objects.filter(o => !o.bHidden && !o.bSelected)
+    },
+    toggleSelect (i) {
+      this.isMove = true
+
+      if (this.objects[i].bSelected) {
+        this.objects[i].bSelected = false
+      } else {
+        this.objects[i].bSelected = true
+      }
+
+      // 選択状態配列を更新
+      this.updateSelect()
     },
     // マウスのクリックが終わった段階で初期化
-    mouseUp (e) {
+    mouseUp () {
       this.isMove = false
-      this.beforeMouseX = null
-      this.beforeMouseY = null
-      e.preventDefault()
+      if (this.beforeMouseX && this.beforeMouseY) {
+        this.beforeMouseX = null
+        this.beforeMouseY = null
+      }
+      if (this.activeDroppable) {
+        this.selected.forEach(o => {
+          o.bHidden = true
+        })
+        this.activeDroppable.bDropping = false
+        this.activeDroppable = null
+        this.clearSelect()
+      }
+      this.movingObjectCallbacks.forEach(o => {
+        o.end()
+      })
+      this.movingObjectCallbacks = []
+      if (this.lastTouchElement) {
+        let event = document.createEvent("mouseevents")
+        event.initEvent("mouseleave", true, true)
+        this.lastTouchElement.dispatchEvent(event)
+        this.lastTouchElement = null
+      }
+      this.clearLongTap()
+    },
+    clearSelect () {
+      this.selected.forEach(o => o.bSelected = false)
+      
+      // 選択状態配列を更新
+      this.updateSelect()
+    },
+    addMovingObject (move, moveEnd) {
+      console.log('addMovingObject')
+      this.movingObjectCallbacks.push({move: move, end:moveEnd})
     },
     // move中は前回まで動かした差分を取りながら座標を変化させていく
     mouseMove (e) {
-      if (!this.isMove) return
-      var mouseX = e.offsetX * this.ratio + this.dx
-      var mouseY = e.offsetY * this.ratio + this.dy
-      var dx = 0
-      var dy = 0
+      //if (!this.isMove || !this.selected) return
+      let mouseX = 0
+      let mouseY = 0
+
+      switch (e.type) {
+        case 'mousemove':
+          mouseX = e.offsetX * this.ratio + this.dx
+          mouseY = e.offsetY * this.ratio + this.dy
+          break
+        case 'touchmove':
+          if (e.touches.length) {
+            mouseX = e.touches[0].pageX * this.ratio + this.dx
+            mouseY = e.touches[0].pageY * this.ratio + this.dy
+
+            if (this.lastTouchElement !== e.touches[0].target) {
+              let event = document.createEvent("mouseevents")
+              event.initEvent("mouseenter", true, true)
+              e.touches[0].target.dispatchEvent(event)
+
+              if (this.lastTouchElement) {
+                event = document.createEvent("mouseevents")
+                event.initEvent("mouseleave", true, true)
+                this.lastTouchElement.dispatchEvent(event)
+              }
+
+              this.lastTouchElement = e.touches[0].target
+            }
+          }
+          break
+      }
+
+      let dx = 0
+      let dy = 0
       if (this.beforeMouseX && this.beforeMouseY) {
           dx = mouseX - this.beforeMouseX
           dy = mouseY - this.beforeMouseY
       }
       this.beforeMouseX = mouseX
       this.beforeMouseY = mouseY
-      var tempX = dx + Number(this.objects[this.selectIdx].x)
-      var tempY = dy + Number(this.objects[this.selectIdx].y)
-      if (tempX > 0) this.objects[this.selectIdx].x = tempX
-      if (tempY > 0) this.objects[this.selectIdx].y = tempY
-      e.preventDefault()
+      /*this.selected.forEach(o => {
+        let tempX = dx + Number(o.x)
+        let tempY = dy + Number(o.y)
+        if (tempX > 10 - o.w) o.x = tempX
+        if (tempY > 10 - o.h) o.y = tempY
+      })*/
+      console.log('mouseMove' + e.type)
+      this.movingObjectCallbacks.forEach(o => {
+        o.move(dx, dy)
+      })
+      //e.preventDefault()
     },
-    flipCard (e, i) {
-      /*if (this.objects[i].bReverse) {
-        this.objects[i].x = this.objects[i].x1
-        this.objects[i].y = this.objects[i].y1
-        this.objects[i].clipPath = this.objects[i].clipPath1
+    flipCard (i) {
+      if (this.selected) {
+        this.selected.forEach(o => {
+          o.faceIndex = (o.faceIndex + 1) % o.faces.length
+          o.bReverse = !o.bReverse
+        })
       } else {
-        this.objects[i].x = this.objects[i].x2
-        this.objects[i].y = this.objects[i].y2
-        this.objects[i].clipPath = this.objects[i].clipPath2
-      }*/
-      this.objects[i].faceIndex = (this.objects[i].faceIndex + 1) % this.objects[i].faces.length
-      this.objects[i].bReverse = !this.objects[i].bReverse
+        this.objects[i].faceIndex = (this.objects[i].faceIndex + 1) % this.objects[i].faces.length
+        this.objects[i].bReverse = !this.objects[i].bReverse
+      }
     }
   }
 }
